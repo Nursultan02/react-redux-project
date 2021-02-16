40c4f12d595c3f6137e7a412fce1b372cfc4ea57
Nursultan02 <nur.bek.aru.bag@gmail.com>
2021-02-17 01:01:36 +0600

    cleaning the project to start

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,9 @@
 import './App.css';
 
-import { useDispatch, useSelector } from 'react-redux';
-import { addNumber, subNumber } from './store/actions/actions';
-
 const App = () => {
-  const dispatch = useDispatch();
-  const count = useSelector((state) => state.countReducer.count);
-
   return (
     <div className='App'>
-      <h1>{count}</h1>
-      <div>
-        <h2>
-          <button onClick={() => dispatch(addNumber())}>+</button>
-        </h2>
-        <h2>
-          <button onClick={() => dispatch(subNumber())}>-</button>
-        </h2>
-      </div>
+      <h1>We are starting our project</h1>
     </div>
   );
 };
